5feed538fecc3709e7ec95984c0ef2c9880624cb
Manasi Sasane <manasisasane1999@gmail.com>
2024-07-10 19:22:32 +0530

    model commit

---
+++ FoodRecipe/src/main/java/com/FoodRecipe/model/VegRecipe.java
@@ -0,0 +1,17 @@
+package com.FoodRecipe.model;
+
+public class VegRecipe {
+
+	private String recipeName;
+
+	public String getRecipeName() {
+		return recipeName;
+	}
+
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
+	}
+	
+	
+	
+}

adc32966fba581a9f33f606fab6f87af9c4b2435
Manasi Sasane <manasisasane1999@gmail.com>
2024-07-10 19:18:33 +0530

    foodrecipe added

---
+++ FoodRecipe/src/main/java/com/FoodRecipe/FoodRecipeApplication.java
@@ -0,0 +1,14 @@
+package com.FoodRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecipeApplication.class, args);
+		System.out.println("Food Recipe Application Started...!!!");
+	}
+
+}

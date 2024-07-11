032a83d205d9b6e72eca896d8c501e99ee47df5b
Manasi Sasane <manasisasane1999@gmail.com>
2024-07-11 20:27:37 +0530

    modified model class

---
+++ FoodRecipe/src/main/java/com/FoodRecipe/model/Recipe.java
@@ -0,0 +1,66 @@
+package com.FoodRecipe.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "recipe")
+public class Recipe {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer recipeID;
+	@Column(name = "recipe_name")
+	private String recipeName;
+	@Column(name = "recipe_description")
+	private String recipeDescription;
+	@Column(name ="recipeDate")
+	private LocalDate recipeDate;
+	@Column(name="authorName")
+	private String authorName;
+
+	public String getRecipeName() {
+		return recipeName;
+	}
+
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
+	}
+
+	public String getRecipeDescription() {
+		return recipeDescription;
+	}
+
+	public void setRecipeDiscription(String recipeDescription) {
+		this.recipeDescription = recipeDescription;
+	}
+
+	public LocalDate getRecipeDate() {
+		return recipeDate;
+	}
+
+	public void setRecipeDate(LocalDate recipeDate) {
+		this.recipeDate = recipeDate;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	@Override
+	public String toString() {
+		return "Recipe [recipeName=" + recipeName + ", recipeDescription=" + recipeDescription + ", recipeDate="
+				+ recipeDate + ", authorName=" + authorName + "]";
+	}
+
+}
--- FoodRecipe/src/main/java/com/FoodRecipe/model/VegRecipe.java
+++
@@ -1,8 +0,0 @@
-package com.FoodRecipe.model;
-
-public class VegRecipe {
-
-	private String vegRecipeName;
-	private String vegRecipeDiscription;
-	
-}
